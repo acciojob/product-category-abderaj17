4a525d3d10fce3ac4b1fbbc1dd6f426dc760b32e
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-24 21:13:45 +0530

    om

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+const [selectedCategory, setSelectedCategory] = useState("Male");
 
-
-  const male = ["Shirt", "Facial-grooming kits", "Hair styling", "Jeans", "Shoes"];
-  const female = ["Tops", "Skirts", "Makeup items", "Jewellery", "Sandals"];
+const products = {
+  Male: ["Shirt", "Facial-grooming kits", "Hair styling", "Jeans", "Shoes"],
+  Female: ["Tops", "Skirts", "Makeup items", "Jewellery", "Sandals"],
+};
 
   
+
   return (
     <div>
-       <select>
-        <option>Male</option>
-        <option>Female</option>
+       <select onChange={(e) => setSelectedCategory(e.target.value)}>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
        </select>
+
+       <ul>
+       {products[selectedCategory].map((item, index) =>(
+        <li key={index}>{item}</li>
+       ))}
+        </ul>
     </div>
   )
 }
