2f6d047dde38273003dd9fca50e3232578b143e4
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-24 21:03:05 +0530

    com

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,18 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+
+  const male = ["Shirt", "Facial-grooming kits", "Hair styling", "Jeans", "Shoes"];
+  const female = ["Tops", "Skirts", "Makeup items", "Jewellery", "Sandals"];
+
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+       <select>
+        <option>Male</option>
+        <option>Female</option>
+       </select>
     </div>
   )
 }
